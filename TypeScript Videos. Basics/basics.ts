22b094e5e996f56a5a1c87eca971afb6540115d5
Mary Linda Bittle <140030405+MLBittle@users.noreply.github.com>
2023-09-22 10:48:55 -0500

    Basics and Primitives to my TypeScript knowledge
    numbers
    strings
    booleans

---
+++ TypeScript Videos. Basics/basics.ts
@@ -0,0 +1,23 @@
+// Primitives: number, string, boolean, null, undefined
+// More complex types: arrays, objects
+// Function types, parameters
+
+
+// Primitives ( use lower case)
+
+let age: number; // can assign a value here after number = 24;
+
+age = 12.1; //works
+
+age = '12'; // error, because a string is not a number
+
+
+
+let userName: string;
+
+userName = 'Max';
+
+
+let isInstructor: boolean;
+
+isInstructor = true;
